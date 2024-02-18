23276585a331cdee28b9c343c58a3be2faa4891a
igordev97 <business.igorstojadinovic97@gmail.com>
2024-02-18 16:27:07 +0100

    Change if src is equal empty string then go to besic page with

--- php_logic/adminsearch.php
+++ php_logic/adminsearch.php
@@ -1,5 +1,9 @@
 <?php
     $src = $_GET["src"];
+    if($src == ""){
+        header("Location:../admin.php");
+        exit();
+    }
     header("Location:../admin.php?src=$src");
     exit();
 
--- php_logic/search.php
+++ php_logic/search.php
@@ -1,6 +1,12 @@
 <?php
 
 $src = $_GET["src"];
+
+if($src ==""){
+    header("Location:../");
+exit();
+}
 header("Location:../?src=$src");
 exit();
+
 ?>
